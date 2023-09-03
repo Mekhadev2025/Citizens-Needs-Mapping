c37e8b97d7ce3c227a62f0eac0fce080e880b0b0
Mekha <mekhalnair@gmail.com>
2023-09-03 11:51:22 +0530

    sdfg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
-import Survey from "./pages/AttemptSurvey/Survey.jsx"
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Survey from "./pages/AttemptSurvey/Survey.jsx";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <Survey/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/survey" element={<Survey />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
