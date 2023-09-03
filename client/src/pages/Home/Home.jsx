e754cfd1204b165c3b569279c17af922bc868356
Mekha <mekhalnair@gmail.com>
2023-09-03 13:19:03 +0530

    sdfghnjm

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import "../Home/Home.css"
-import heroImage from "../../assets/hero.svg"
-
+import React from "react";
+import "../Home/Home.css";
+import heroImage from "../../assets/hero.svg";
+import {Link} from "react-router-dom"
 const Home = () => {
   return (
     <div>
-        <div className="hero">
-            <h1 className='heroTitle1'>
-                Citizen
-            </h1>
-            <h1 className="heroTitle2">
-                Needs Mapping
-            </h1>
-            <p className='heroContent'>Discover, Understand, and Address the Needs of  Citizens</p>
-            <button className="heroButton">Attempt Survey</button>
-        </div>
+      <div className="hero">
+        <h1 className="heroTitle1">Citizen</h1>
+        <h1 className="heroTitle2">Needs Mapping</h1>
+        <p className="heroContent">
+          Discover, Understand, and Address the Needs of Citizens
+        </p>
+        <Link to="/survey">
+          <button className="heroButton">Attempt Survey</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
