f5ccce469f53fa829fc8f5424bf34397d2d772d5
Mekha <mekhalnair@gmail.com>
2023-09-03 13:30:06 +0530

    ddd

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "../Home/Home.css";
-import Hero from "../../components/Hero/Hero"
+import Hero from "../../components/Hero/Hero";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <p className="homeDescription">
+        Our Citizen Needs Project is an initiative led by Governance Innovation
+        Labs (GIL), an organization deeply committed to the power of community
+        and collective action. We are dedicated to creating a platform that
+        empowers citizens to voice their needs, no matter how basic or complex
+        they may be. Through collaborative efforts, we aim to build stronger,
+        more responsive communities that prioritize the well-being of every
+        member
+      </p>
     </div>
   );
 };
