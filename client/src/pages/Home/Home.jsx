295c1fc13d0672a4183e58d0109b6c5d90a7b5fa
Mekha <mekhalnair@gmail.com>
2023-09-03 13:25:10 +0530

    working state

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import "../Home/Home.css";
-import heroImage from "../../assets/hero.svg";
-import {Link} from "react-router-dom"
+import Hero from "../../components/Hero/Hero"
+
 const Home = () => {
   return (
     <div>
-      <div className="hero">
-        <h1 className="heroTitle1">Citizen</h1>
-        <h1 className="heroTitle2">Needs Mapping</h1>
-        <p className="heroContent">
-          Discover, Understand, and Address the Needs of Citizens
-        </p>
-        <Link to="/survey">
-          <button className="heroButton">Attempt Survey</button>
-        </Link>
-      </div>
+      <Hero/>
     </div>
   );
 };
