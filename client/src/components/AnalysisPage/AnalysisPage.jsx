a26e6066b62f929410fe66950facbbf71233e9e8
Mekha <mekhalnair@gmail.com>
2023-09-03 23:19:17 +0530

    sdfgh

--- client/src/components/AnalysisPage/AnalysisPage.jsx
+++ client/src/components/AnalysisPage/AnalysisPage.jsx
@@ -1,11 +1,15 @@
  // src/components/AnalysisPage.js
 import React from 'react';
-
+import "../AnalysisPage/Analysis.css"
 const AnalysisPage = ({ selectedDistrict }) => {
   return (
     <div>
-      <h2>Analysis for {selectedDistrict}</h2>
-    
+      <h2 className='districtName'>{selectedDistrict}</h2>
+      <div className="btnGrp">
+        <button className="basic">Basic</button>
+        <button className="std">Standard</button>
+        <button className="premium">Premium</button>
+      </div>
     </div>
   );
 };
