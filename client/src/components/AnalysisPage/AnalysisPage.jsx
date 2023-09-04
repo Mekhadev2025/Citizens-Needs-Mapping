9008a6fc305149ac10a2d6256c705929c1d1e4ed
Mekha <mekhalnair@gmail.com>
2023-09-04 17:59:10 +0530

    yes done

--- client/src/components/AnalysisPage/AnalysisPage.jsx
+++ client/src/components/AnalysisPage/AnalysisPage.jsx
@@ -1,185 +1,142 @@
- // src/components/AnalysisPage.js
-import axios from "axios"
-import React,{useState,useEffect} from 'react';
-import "../AnalysisPage/Analysis.css"
-import Card from "../../components/Card/Card"
- 
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import "../AnalysisPage/Analysis.css";
+import Card from "../../components/Card/Card";
+
 const AnalysisPage = ({ selectedDistrict }) => {
-
-  const basicNeeds=['Primary School', 'Public Toilet', 'Street Light', 'Health clinic', 'Municipal Water Supply', 'Road Reconstruction', 'Avoid Powercuts']
-  console.log("selceted",selectedDistrict)
-  const[propsData,setPropsData]=useState({})
-  const [selectedButton,setSelectedButton]=useState("basic")
-
-  const fetchData=async(districtId) =>{
+  console.log("selected", selectedDistrict);
+  const [propsData, setPropsData] = useState({});
+  const [selectedButton, setSelectedButton] = useState("basic");
+  const [basicNeeds, setBasicNeeds] = useState([]); // Initialize basicNeeds state
+  const [stdNeeds, setStdNeeds] = useState([]); // Initialize stdNeeds state
+  const [preNeeds, setPreNeeds] = useState([]); 
+  const fetchData = async (districtId) => {
     try {
       const response = await axios.get(`https://citizens-needs-mapping-whzj.vercel.app/api/surveys/${districtId}`);
       const data = response.data;
-  
-      setPropsData(data)
-   
+      setPropsData(data);
+      console.log("data=", data);
+      // Update basicNeeds and stdNeeds states with data
+      setBasicNeeds(data.basicNeeds);
+      setStdNeeds(data.standardNeeds);
+      setPreNeeds(data.premiumNeeds);
     } catch (error) {
-
       console.error('Error fetching data:', error);
     }
-  }
+  };
+
   useEffect(() => {
     fetchData(selectedDistrict);
   }, [selectedDistrict]);
 
+  // Basic needs categories and counts
+  const categories = [
+    "Primary School",
+    "Public Toilet",
+    "Street Light",
+    "Health clinic",
+    "Municipal Water Supply",
+    "Road Reconstruction",
+    "Avoid Powercuts"
+  ];
 
-  const basic=propsData.basicNeeds;
-  console.log("baisic",basic)
+  const categoryCounts = {};
 
-  const basicNeed=[{
-    need:"Public School",
-    vote:"13",
-    value:"75"
-  },
-  {
-    need:"Public Toilet",
-    vote:"15",
-    value:"80"
-  },
-  {
-    need:"StreetLight",
-    vote:"13",
-    value:"75"
-  },
-  {
-    need:"Health Clinic",
-    vote:"10",
-    value:"60"
-  },
-  {
-    need:"Municipal Water Supply",
-    vote:"3",
-    value:"10"
-  },
-  {
-    need:"Road Reconstruction",
-    vote:"7",
-    value:"30"
-  },{
-    need:"Avoid Powercuts",
-    vote:"20",
-    value:"90"
-  }
-  
-]
+  categories.forEach(category => {
+    categoryCounts[category] = 0;
+  });
+
+  basicNeeds.forEach(need => {
+    if (categoryCounts.hasOwnProperty(need)) {
+      categoryCounts[need]++;
+    }
+  });
+
+  // Standard needs categories and counts
+  const categories2 = [
+    "Public Library",
+    "Taxi Service",
+    "Public Parks and Playgrounds",
+    "Traffic Control",
+    "Night Patrol",
+    "Bus Waiting Shed",
+    "Better Disaster Prevention Schemes"
+  ];
+
+  const categoryCounts2 = {};
+
+  categories2.forEach(category => {
+    categoryCounts2[category] = 0;
+  });
+
+  stdNeeds.forEach(need => {
+    if (categoryCounts2.hasOwnProperty(need)) {
+      categoryCounts2[need]++;
+    }
+  });
+  //Premium
+  const categories3 = [
+"Gym",
+"Theatre",
+"Tourist resorts",
+"Metro rail services",
+"Free wifi",
+"Turf"
+  ];
+
+  const categoryCounts3 = {};
+
+  categories3.forEach(category => {
+    categoryCounts3[category] = 0;
+  });
+
+  preNeeds.forEach(need => {
+    if (categoryCounts3.hasOwnProperty(need)) {
+      categoryCounts3[need]++;
+    }
+  });
 
 
-//    Primary School{
 
-//    }, 'Public Toilet', 'Street Light', 'Health clinic', 'Municipal Water Supply', 'Road Reconstruction', 'Avoid Powercuts']
-// ]
 
-const stdNeed=[{
-  need:"Public Library",
-  vote:"13",
-  value:"75"
-},
-{
-  need:"Taxi Service",
-  vote:"15",
-  value:"80"
-},
-{
-  need:"Public Parks And Playgrounds",
-  vote:"13",
-  value:"75"
-},
-{
-  need:"Traffic Control",
-  vote:"10",
-  value:"60"
-},
 
-{
-  need:"Night patrol",
-  vote:"7",
-  value:"30"
-},{
-  need:"Bus Waiitng Shed",
-  vote:"20",
-  value:"90"
-},{
-  need:"Better Disaster Prevention Schemes",
-  vote:"3",
-  value:"10"
-},
 
-]
+  console.log("categoryCounts", categoryCounts);
+  console.log("categoryCounts2", categoryCounts2);
+  console.log("categoryCounts2", categoryCounts3);
+  const handleButton = (btnName) => {
+    setSelectedButton(btnName);
+  };
 
-const preNeed=[{
-  need:"Gym",
-  vote:"11",
-  value:"88"
-},
-{
-  need:"Theatre",
-  vote:"15",
-  value:"30"
-},
-{
-  need:"Tourist Resorts",
-  vote:"13",
-  value:"75"
-},
-{
-  need:"Metro Rail services",
-  vote:"10",
-  value:"60"
-},
-{
-  need:"Free Wifi",
-  vote:"3",
-  value:"10"
-},
-{
-  need:"Turf",
-  vote:"7",
-  value:"30"
-}
-]
-  const handleButton=(btnName)=>{
-   setSelectedButton(btnName)
-  }
   return (
     <div>
       <h2 className='districtName'>{selectedDistrict}</h2>
       <div className="btnGrp">
-        <button  className={`basic ${selectedButton === 'basic' ? 'active-button' : ''}`}
-         onClick={()=>handleButton("basic")}>Basic</button>
-        <button className={`std ${selectedButton === 'standard' ? 'active-button' : ''}`}
-         onClick={()=>handleButton("standard")}>Standard</button>
-        <button className={`premium ${selectedButton === 'premium' ? 'active-button' : ''}`}
-        onClick={()=>handleButton("premium")}>Premium</button>
+        <button className={`basic ${selectedButton === 'basic' ? 'active-button' : ''}`} onClick={() => handleButton("basic")}>Basic</button>
+        <button className={`std ${selectedButton === 'standard' ? 'active-button' : ''}`} onClick={() => handleButton("standard")}>Standard</button>
+        <button className={`premium ${selectedButton === 'premium' ? 'active-button' : ''}`} onClick={() => handleButton("premium")}>Premium</button>
       </div>
       <div className='analysisCard'>
-         <h2 className='analysisTitle'>
-        {selectedButton} Needs
-         </h2>
-         <div className="analysisCont">
-            {/* <Card need="Public School" vote="13" value="75" />
-            <Card need="Public toilet" vote="5" value="50" /> */}
-           {
-              selectedButton==="basic"?(basicNeed.map((item)=>{
-                return <Card need={item.need} vote={item.vote} value={item.value}/>
-              })):(
-                selectedButton==="standard"?(stdNeed.map((item)=>{
-                  return <Card need={item.need} vote={item.vote} value={item.value}/>
-                })):(preNeed.map((item)=>{
-                  return <Card need={item.need} vote={item.vote} value={item.value}/>
-                })
-                  
-                )
-                
-                
-              )
-           }
-
-         </div>
+        <h2 className='analysisTitle'>
+          {selectedButton} Needs
+        </h2>
+        <div className="analysisCont">
+          {
+            selectedButton === "basic" ? (
+              Object.entries(categoryCounts).map((item, index) => (
+                <Card key={index} need={item[0]} vote={item[1]} value={item[1]} />
+              ))
+            ) : selectedButton === "standard" ? (
+              Object.entries(categoryCounts2).map((item, index) => (
+                <Card key={index} need={item[0]} vote={item[1]} value={item[1]} />
+              ))
+            ) : (Object.entries(categoryCounts3).map((item, index) => (
+              <Card key={index} need={item[0]} vote={item[1]} value={item[1]} />
+            ))
+               
+            )
+          }
+        </div>
       </div>
     </div>
   );
