d7ed30e46b6a6c868de6526d4998cf4c243dbbff
Mekha <mekhalnair@gmail.com>
2023-09-04 11:44:32 +0530

    aaaa

--- client/src/components/AnalysisPage/AnalysisPage.jsx
+++ client/src/components/AnalysisPage/AnalysisPage.jsx
@@ -7,6 +7,113 @@ import Card from "../../components/Card/Card"
 const AnalysisPage = ({ selectedDistrict }) => {
   const [selectedButton,setSelectedButton]=useState("basic")
  
+
+
+  const basicNeed=[{
+    need:"Public School",
+    vote:"13",
+    value:"75"
+  },
+  {
+    need:"Public Toilet",
+    vote:"15",
+    value:"80"
+  },
+  {
+    need:"StreetLight",
+    vote:"13",
+    value:"75"
+  },
+  {
+    need:"Health Clinic",
+    vote:"10",
+    value:"60"
+  },
+  {
+    need:"Municipal Water Supply",
+    vote:"3",
+    value:"10"
+  },
+  {
+    need:"Road Reconstruction",
+    vote:"7",
+    value:"30"
+  },{
+    need:"Avoid Powercuts",
+    vote:"20",
+    value:"90"
+  }
+  
+]
+
+const stdNeed=[{
+  need:"Public Library",
+  vote:"13",
+  value:"75"
+},
+{
+  need:"Taxi Service",
+  vote:"15",
+  value:"80"
+},
+{
+  need:"Public Parks And Playgrounds",
+  vote:"13",
+  value:"75"
+},
+{
+  need:"Traffic Control",
+  vote:"10",
+  value:"60"
+},
+
+{
+  need:"Night patrol",
+  vote:"7",
+  value:"30"
+},{
+  need:"Bus Waiitng Shed",
+  vote:"20",
+  value:"90"
+},{
+  need:"Better Disaster Prevention Schemes",
+  vote:"3",
+  value:"10"
+},
+
+]
+
+const preNeed=[{
+  need:"Gym",
+  vote:"11",
+  value:"88"
+},
+{
+  need:"Theatre",
+  vote:"15",
+  value:"30"
+},
+{
+  need:"Tourist Resorts",
+  vote:"13",
+  value:"75"
+},
+{
+  need:"Metro Rail services",
+  vote:"10",
+  value:"60"
+},
+{
+  need:"Free Wifi",
+  vote:"3",
+  value:"10"
+},
+{
+  need:"Turf",
+  vote:"7",
+  value:"30"
+}
+]
   const handleButton=(btnName)=>{
    setSelectedButton(btnName)
   }
@@ -26,9 +133,23 @@ const AnalysisPage = ({ selectedDistrict }) => {
         {selectedButton} Needs
          </h2>
          <div className="analysisCont">
-            <Card need="Public School" vote="13" value="75" />
-            <Card need="Public toilet" vote="5" value="50" />
-           
+            {/* <Card need="Public School" vote="13" value="75" />
+            <Card need="Public toilet" vote="5" value="50" /> */}
+           {
+              selectedButton==="basic"?(basicNeed.map((item)=>{
+                return <Card need={item.need} vote={item.vote} value={item.value}/>
+              })):(
+                selectedButton==="standard"?(stdNeed.map((item)=>{
+                  return <Card need={item.need} vote={item.vote} value={item.value}/>
+                })):(preNeed.map((item)=>{
+                  return <Card need={item.need} vote={item.vote} value={item.value}/>
+                })
+                  
+                )
+                
+                
+              )
+           }
 
          </div>
       </div>
