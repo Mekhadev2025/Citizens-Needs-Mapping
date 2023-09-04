3537c9866633335e4af53bb916c10ddacdac80fb
Mekha <mekhalnair@gmail.com>
2023-09-04 15:49:23 +0530

    aaa

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port =  5000;
 
 
 app.use(express.json());
